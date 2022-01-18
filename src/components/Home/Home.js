490a63775837de67194048a99b7d3b3638275415
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2022-01-19 00:24:57 +0600

    Movie card added in ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,17 +2,20 @@ import React, { useEffect } from 'react';
 import movieApi from '../../common/apis/movieApi';
 import MovieListing from '../MovieListing/MovieListing';
 import { APIKey } from '../../common/apis/MovieApiKey'
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
+  const movieText = "Marvel";
+  const dispatch = useDispatch();
   useEffect(() => {
-    const movieText = "Marvel";
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.log("Err:", err);
         });
-      console.log("response from api", response);
+      dispatch(addMovies(response.data))
     };
     fetchMovies();
   }, []);
